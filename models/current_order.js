74beeffea970acb41834458accf501fcdd0b9309
Ninda Khoirunnisa <nindakhoirunnisa.nk@gmail.com>
2023-01-04 14:28:30 +0000

    delete partners.json

---
+++ models/current_order.js
@@ -0,0 +1,42 @@
+const { Number, now } = require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const storeAddress = new Schema({
+  unit_no: {
+    type: String,
+    required: true
+  },
+  street: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  postcode: {
+    type: String,
+    required: true
+  },
+  location: geojsonSchema
+})
+
+const storeSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  address: storeAddress
+},
+{
+  versionKey: false
+});
+
+const Store = mongoose.model('Store', storeSchema);
+
+module.exports = Store;
