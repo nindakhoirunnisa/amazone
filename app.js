11de290139a6a93f20baf12ebf125ef107e9986e
Ninda Khoirunnisa <nindakhoirunnisa.nk@gmail.com>
2023-01-05 12:55:35 +0000

    fresh product

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const partnersRouter = require('./routes/partners');
 const customersRouter = require('./routes/customers');
 const customerAddressesRouter = require('./routes/customer_addresses');
-const productCatalogsRouter = require('./routes/product_catalogs');
+const productCatalogsRouter = require('./routes/fresh_products');
 const productDescRouter = require('./routes/product_descriptions');
 const storesRouter = require('./routes/stores');
 const currentOrdersRouter = require('./routes/current_orders')
@@ -30,8 +30,9 @@ app.use(express.json());
 app.use('/api/partners', partnersRouter);
 app.use('/api/customers', customersRouter);
 app.use('/api/customer_addresses', customerAddressesRouter);
-app.use('/api/products', productCatalogsRouter);
-app.use('/api/products/description', productDescRouter);
+app.use('/api/product', productCatalogsRouter);
+app.use('/api/product/fresh', productCatalogsRouter);
+app.use('/api/product/description', productDescRouter);
 app.use('/api/stores', storesRouter);
 app.use('/api/order/current_orders', currentOrdersRouter);
 
