3378f9299d0f79ce92cbe45d9f7a61c8ff8f7685
Ninda Khoirunnisa <nindakhoirunnisa.nk@gmail.com>
2023-01-13 12:43:04 +0000

    export data, paginate get customer

--- routes/customers.js
+++ routes/customers.js
@@ -5,12 +5,14 @@ const Customer = require('../models/customer');
 module.exports = router; 
 
 router.get('/', (req, res, next) => {
-  Customer
-    .aggregate([
-      { $limit: 100 }
-    ])
-    .then(cstmr => res.send(cstmr))
-    .catch(err => next(err));
+  const size = parseInt(req.query.size)
+  let page = parseInt(req.query.page) >= 1 ? parseInt(req.query.page) : 1;
+
+  page = page - 1
+
+  Customer.find({}).limit(size).skip(size*page).exec().then(customers => {
+    res.json({customers, page: page+1, size: size})
+  })
 });
 
 router.post('/', (req, res, next) => {
